777043f29f6eb2a19745c0b283fe84d2fc53e6e3
sachingour <gour.sachin.sg@gmail.com>
2025-05-26 11:27:14 +0530

    Api Develop- Post user

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const { connectDB } = require("./config/db");
-const { adminAuth, userAuth } = require("./middlewares/authMiddleware");
+const { User } = require("./models/user");
+app.use(express.json());
+
+app.post("/singup", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    res.send("user saved Succesfully!");
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("data not Ok");
+  }
+});
 
 connectDB()
   .then(() => {
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://goursachinsg:<password>@cluster0.sxnebur.mongodb.net/dev_Tinder"
+    "mongodb+srv://goursachinsg:DzvufSu6K68OkYUb@cluster0.sxnebur.mongodb.net/dev_Tinder"
   );
 };
 
