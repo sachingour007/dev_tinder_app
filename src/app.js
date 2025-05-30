b7f5c6af59c398b288acc96fba93e8f7923a0b05
sachingour <gour.sachin.sg@gmail.com>
2025-05-30 23:05:24 +0530

    Played with some Curd Apis

--- src/app.js
+++ src/app.js
@@ -5,15 +5,56 @@ const { User } = require("./models/user");
 const dotenv = require("dotenv").config();
 app.use(express.json());
 
+//Post Api Singup
 app.post("/singup", async (req, res) => {
   const user = new User(req.body);
-
   try {
     await user.save();
     res.send("user saved Succesfully!");
   } catch (error) {
     console.log(error);
     res.status(404).send("data not Ok");
+  }
+});
+
+//Get Api
+
+app.get("/user", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.send(user);
+  } catch (error) {
+    console.log(error);
+    res.status(404).send("Somthing Went Wrong!");
+  }
+});
+
+//Delete Api
+
+app.delete("/user", async (req, res) => {
+  const id = req.body.id;
+  try {
+    const user = await user.findByIdAndDelete(id);
+    await User.findByIdAndDelete({ _id: id });
+    res.send(" User Deleted Successfully!");
+  } catch (error) {
+    res.status(404).send("Somthing Went Wrong!");
+  }
+});
+
+//Update Api
+
+app.patch("/user", async (req, res) => {
+  const id = req.body.id;
+  const data = req.body;
+
+  try {
+    const newUser = await User.findByIdAndUpdate({ _id: id }, data, {
+      returnDocument: "after",
+    });
+    res.send({ res: " User Update Successfully!", newUser });
+  } catch (error) {
+    res.status(404).send("Somthing Went Wrong!");
   }
 });
 
