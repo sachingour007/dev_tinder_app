314fcd694882e3ea3d0025dde9c79faffd465e01
sachingour <gour.sachin.sg@gmail.com>
2025-06-19 23:02:19 +0530

    pre function add in request model

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -24,6 +24,14 @@ const connectionRequestSchema = new mongoose.Schema(
   }
 );
 
+connectionRequestSchema.pre("save", function (next) {
+  const connectionRequest = this;
+  if (connectionRequest.fromUserId.equals(connectionRequest.toUserId)) {
+    throw new Error("Cannot send connection request to yourself!");
+  }
+  next();
+});
+
 const ConnectionRequestModel = new mongoose.model(
   "ConnectionRequestModel",
   connectionRequestSchema
