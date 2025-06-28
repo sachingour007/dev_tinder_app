25450e7faeac236a6729b6100d24eae3a556db62
sachingour <gour.sachin.sg@gmail.com>
2025-06-28 20:11:05 +0530

    connection requestion all api

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/authMiddleware");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const USER_SAFE_DATA = "firstName lastName photoUrl gender age about";
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
@@ -10,16 +11,35 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
     const connectionRequests = await ConnectionRequestModel.find({
       toUserId: loggedInUser._id,
       status: "interested",
-    }).populate("fromUserId", [
-      "firstName",
-      "lastName",
-      "photoUrl",
-      "gender",
-      "age",
-      "about",
-    ]);
+    }).populate("fromUserId", USER_SAFE_DATA);
 
     res.json({ message: "Data Fatch Successfully", connectionRequests });
+  } catch (error) {
+    res.status(404).send({ Error: error.message });
+  }
+});
+
+userRouter.get("/user/all/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequest = await ConnectionRequestModel.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const requestUserDetails = connectionRequest.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+
+    res.json({ message: "Data Fatch Successfully", requestUserDetails });
   } catch (error) {
     res.status(404).send({ Error: error.message });
   }
