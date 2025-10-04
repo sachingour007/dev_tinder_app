5a389a3ff0242426a11284645137131f292721f0
sachingour <gour.sachin.sg@gmail.com>
2025-10-04 19:03:04 +0530

    secure false

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -8,95 +8,95 @@ const USER_SAFE_DATA = "firstName lastName photoUrl gender age about skills";
 
 //Sign-up API
 authRouter.post("/signup", async (req, res) => {
-  try {
-    //Validation
-    validationSingupData(req);
-    const { firstName, lastName, emailId, password, age, gender } = req.body;
+	try {
+		//Validation
+		validationSingupData(req);
+		const { firstName, lastName, emailId, password, age, gender } = req.body;
 
-    //hash the password
-    const passwordHash = await bcrypt.hash(password, 10);
-    const user = new User({
-      firstName,
-      lastName,
-      emailId,
-      password: passwordHash,
-      age,
-      gender,
-    });
+		//hash the password
+		const passwordHash = await bcrypt.hash(password, 10);
+		const user = new User({
+			firstName,
+			lastName,
+			emailId,
+			password: passwordHash,
+			age,
+			gender,
+		});
 
-    const token = await user.getJwt();
-    res.cookie("token", token, {
-      // secure: true,
-      httpOnly: true,
-      expires: new Date(Date.now() + 24 * 3600000),
-    });
+		const token = await user.getJwt();
+		res.cookie("token", token, {
+			secure: false,
+			httpOnly: true,
+			expires: new Date(Date.now() + 24 * 3600000),
+		});
 
-    const signUser = await user.save();
-    res.send({ res: "user saved Succesfully!", signUser });
-  } catch (error) {
-    res.status(404).send({ Error: error.message });
-  }
+		const signUser = await user.save();
+		res.send({ res: "user saved Succesfully!", signUser });
+	} catch (error) {
+		res.status(404).send({ Error: error.message });
+	}
 });
 
 //login API
 authRouter.post("/login", async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-    const userDetails = await User.findOne({ emailId });
+	try {
+		const { emailId, password } = req.body;
+		const userDetails = await User.findOne({ emailId });
 
-    if (!userDetails) {
-      return res.status(401).send({ error: "Invalid credentials !!" });
-    }
-    const isValidPassword = await userDetails.validatePassword(password);
+		if (!userDetails) {
+			return res.status(401).send({ error: "Invalid credentials !!" });
+		}
+		const isValidPassword = await userDetails.validatePassword(password);
 
-    if (!isValidPassword) {
-      return res.status(401).send({ error: "Invalid credentials !!" });
-    }
-    const token = await userDetails.getJwt();
-    res.cookie("token", token, {
-      // secure: true,
-      httpOnly: true,
-      expires: new Date(Date.now() + 24 * 3600000),
-    });
+		if (!isValidPassword) {
+			return res.status(401).send({ error: "Invalid credentials !!" });
+		}
+		const token = await userDetails.getJwt();
+		res.cookie("token", token, {
+			secure: false,
+			httpOnly: true,
+			expires: new Date(Date.now() + 24 * 3600000),
+		});
 
-    const user = {
-      id: userDetails._id,
-      firstName: userDetails.firstName,
-      lastName: userDetails.lastName,
-      photoUrl: userDetails.photoUrl,
-      gender: userDetails.gender,
-      age: userDetails.age,
-      about: userDetails.about,
-      skills: userDetails.skills,
-    };
-    res.json({
-      user,
-    });
-  } catch (error) {
-    res.status(500).send({ error: "Something went wrong on the server" });
-  }
+		const user = {
+			id: userDetails._id,
+			firstName: userDetails.firstName,
+			lastName: userDetails.lastName,
+			photoUrl: userDetails.photoUrl,
+			gender: userDetails.gender,
+			age: userDetails.age,
+			about: userDetails.about,
+			skills: userDetails.skills,
+		};
+		res.json({
+			user,
+		});
+	} catch (error) {
+		res.status(500).send({ error: "Something went wrong on the server" });
+	}
 });
 
 //Logout API
 authRouter.post("/logout", (req, res) => {
-  res.clearCookie("token", {
-    httpOnly: true,
-    // secure: true,
-    sameSite: "strict",
-  });
-  res.status(200).json({ message: "User logout successful!" });
+	res.clearCookie("token", {
+		httpOnly: true,
+		secure: false,
+		sameSite: "strict",
+	});
+	res.status(200).json({ message: "User logout successful!" });
 });
 
 //Delete Api
 authRouter.delete("/user", async (req, res) => {
-  const id = req.body.id;
-  try {
-    const user = await user.findByIdAndDelete(id);
-    await User.findByIdAndDelete({ _id: id });
-    res.send(" User Deleted Successfully!");
-  } catch (error) {
-    res.status(404).send("Somthing Went Wrong!");
-  }
+	const id = req.body.id;
+	try {
+		const user = await user.findByIdAndDelete(id);
+		await User.findByIdAndDelete({ _id: id });
+		res.send(" User Deleted Successfully!");
+	} catch (error) {
+		res.status(404).send("Somthing Went Wrong!");
+	}
 });
 
 module.exports = authRouter;
